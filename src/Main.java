c1b7b74c0a9147df45ed25afbfc699c4556a271d
Sonyaandr <andreevasd8@gmail.com>
2024-06-04 21:19:14 +0300

    Сделано дз

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание 1");
-        int[] array1 = new int[] {1, 2, 3};
+        int[] array1 = new int[]{1, 2, 3};
         double[] array2 = {1.57, 7.654, 9.986};
         int[] array3 = new int[]{2, 4, 6, 7};
 
@@ -32,7 +32,7 @@ public class Main {
         }
 
         System.out.println("Задание 3 ");
-        for (int index = array1.length-1; index > -1; index--) {
+        for (int index = array1.length - 1; index > -1; index--) {
             if (index == 0) {
                 System.out.println(array1[index]);
                 break;
@@ -40,7 +40,7 @@ public class Main {
             System.out.print(array1[index] + ", ");
         }
 
-        for (int index = array2.length-1; index > -1 ; index--) {
+        for (int index = array2.length - 1; index > -1; index--) {
             if (index == 0) {
                 System.out.println(array2[index]);
                 break;
@@ -48,7 +48,7 @@ public class Main {
             System.out.print(array2[index] + ", ");
         }
 
-        for (int index = array3.length-1; index > -1; index--) {
+        for (int index = array3.length - 1; index > -1; index--) {
             if (index == 0) {
                 System.out.println(array3[index]);
                 break;
@@ -58,19 +58,19 @@ public class Main {
 
         System.out.println("Задание 4 ");
         for (int index = 0; index < array1.length; index++) {
-            if ((index == array1.length - 1) && (array1[index]%2 != 0)) {
-                array1[index] +=1;
+            if ((index == array1.length - 1) && (array1[index] % 2 != 0)) {
+                array1[index] += 1;
                 System.out.println(array1[index]);
                 break;
             }
-            if ((index == array1.length - 1) && (array1[index]%2 == 0)) {
+            if ((index == array1.length - 1) && (array1[index] % 2 == 0)) {
                 System.out.println(array1[index]);
                 break;
             }
-            if ((array1[index]%2 == 0)) {
+            if ((array1[index] % 2 == 0)) {
                 System.out.print(array1[index] + ", ");
-            }else {
-                array1[index] +=1;
+            } else {
+                array1[index] += 1;
                 System.out.print(array1[index] + ", ");
             }
         }
